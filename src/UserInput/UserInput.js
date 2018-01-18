b137b6ea41a178d064b2fa4698af1c2eda31ac1b
cthulh <tryglavson@hotmail.co.uk!>
2018-01-18 10:33:48 +0000

    style reset, two comps added

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const userinput = (props) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default userinput;
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const useroutput = (props) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default useroutput;
